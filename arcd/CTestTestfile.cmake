aaafa5e389c9334778c13ec19655361a7f6fbce4
iamirenechao <90223.irene@gmail.com>
2023-01-10 16:33:49 +0800

    555

---
+++ arcd/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/irenechao/hw3/arcd
+# Build directory: /home/irenechao/hw3/arcd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("arcd")
+subdirs("examples")
+subdirs("tests")
